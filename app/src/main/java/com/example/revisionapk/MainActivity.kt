d257d70ccb2cb9403b9a9d3bc85fbfb045cd83f0
Prince Pandey <princepandey2784@gmail.com>
2022-03-06 15:13:39 +0530

    lets start commit 2

--- app/src/main/java/com/example/revisionapk/MainActivity.kt
+++ app/src/main/java/com/example/revisionapk/MainActivity.kt
@@ -3,14 +3,41 @@ package com.example.revisionapk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btton = findViewById(R.id.button2) as Button
-
+        val x=findViewById(R.id.num1) as EditText
+        val y=findViewById(R.id.num2) as EditText
+        var opt=findViewById(R.id.op) as EditText
+        val result=findViewById(R.id.textView4) as TextView
         btton.setOnClickListener{
+            var a:Int=0;var b:Int=0
+             a=x.text.toString().toInt()
+             b=y.text.toString().toInt()
+            var res:Int;
+
+           when(opt.text.toString()){
+               "+" -> {res=a+b}
+               "-" -> {res=a-b}
+               "*" -> {res=a*b}
+               "/" -> {res=a/b}
+               "%" -> {res=a%b}
+               else -> {res=a+b}
+
+           }
+
+
+
+
+
+
+//           res=a+b;
+            result.text=res.toString()
 
         }
     }
