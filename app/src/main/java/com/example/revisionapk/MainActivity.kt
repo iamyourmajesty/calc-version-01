81b920af3d7984c1cacacec5ae4a69a23e0cb171
Prince Pandey <princepandey2784@gmail.com>
2022-03-09 00:45:41 +0530

    lets start commit 2  colors added

--- app/src/main/java/com/example/revisionapk/MainActivity.kt
+++ app/src/main/java/com/example/revisionapk/MainActivity.kt
@@ -5,23 +5,87 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btton = findViewById(R.id.button2) as Button
-        val x=findViewById(R.id.num1) as EditText
-        val y=findViewById(R.id.num2) as EditText
-        var opt=findViewById(R.id.op) as EditText
-        val result=findViewById(R.id.textView4) as TextView
-        btton.setOnClickListener{
-            var a:Int=0;var b:Int=0
-             a=x.text.toString().toInt()
-             b=y.text.toString().toInt()
-            var res:Int;
+        var str:String ?= null
 
-           when(opt.text.toString()){
+        //method - 1
+//        val btton = findViewById(R.id.button2) as Button
+//        val x=findViewById(R.id.num1) as EditText
+//        val y=findViewById(R.id.num2) as EditText
+//        var opt=findViewById(R.id.op) as EditText
+//        val result=findViewById(R.id.textView4) as TextView
+//        btton.setOnClickListener{
+//            var a:Int=0;var b:Int=0
+//             a=x.text.toString().toInt()
+//             b=y.text.toString().toInt()
+//            var res:Int;
+//
+//           when(opt.text.toString()){
+//               "+" -> {res=a+b}
+//               "-" -> {res=a-b}
+//               "*" -> {res=a*b}
+//               "/" -> {res=a/b}
+//               "%" -> {res=a%b}
+//               else -> {res=a+b}
+//
+//           }
+//
+
+//  simply adding two numbers//           res=a+b;
+//            result.text=res.toString()
+//
+//        }
+
+        //method - 2
+
+        op.setOnCheckedChangeListener { group, checkedId ->
+            if (checkedId == R.id.radioButton) {
+                Toast.makeText(
+                    this,
+                    radioButton.text.toString() + "  SELECTED",
+                    Toast.LENGTH_SHORT
+                ).show()
+                str="+"
+            }
+            else if(checkedId == R.id.radioButton2)
+            {
+                Toast.makeText(this,radioButton2.text.toString() + "  SELECTED",Toast.LENGTH_SHORT).show()
+                str="-"
+            }
+            else if(checkedId==R.id.radioButton3)
+            {
+                Toast.makeText(this,radioButton3.text.toString() + "  SELECTED",Toast.LENGTH_SHORT).show()
+                str="*"
+            }
+            else if(checkedId == R.id.radioButton4)
+            {
+                Toast.makeText(this,radioButton4.text.toString() +"  SELECTED",Toast.LENGTH_SHORT).show()
+                str="/"
+            }
+            else if(checkedId == R.id.radioButton5)
+            {
+                Toast.makeText(this,radioButton5.text.toString() + "  SELECTED",Toast.LENGTH_SHORT).show()
+                str="%"
+            }
+        }
+
+
+            button2.setOnClickListener{
+                var a:Int ?= null
+                var b:Int ?= null
+
+                 a=num1.text.toString().toInt()
+                 b = num2.text.toString().toInt()
+
+                var res=0
+               //
+                when(str){
                "+" -> {res=a+b}
                "-" -> {res=a-b}
                "*" -> {res=a*b}
@@ -35,10 +99,10 @@ class MainActivity : AppCompatActivity() {
 
 
 
+                textView4.text=res.toString()
 
-//           res=a+b;
-            result.text=res.toString()
+            }
 
-        }
+
     }
 }
